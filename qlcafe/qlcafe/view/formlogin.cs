0ceb99c5bdabb2a27507aa4383dafb42c61b5d7c
LuTae <luwtae@gmail.com>
2016-12-23 03:49:15 +0700

    Add files via upload

---
+++ qlcafe/qlcafe/view/formlogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using qlcafe.view;
+namespace qlcafe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            Frmmain f = new Frmmain();
+            this.Hide();
+            f.ShowDialog();
+           
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
